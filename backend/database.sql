a52dcbb38dad7b34fe4630929573464f92175b95
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-01-08 15:55:37 +0100

    Add stylesheet and more dummy data

--- backend/database.sql
+++ backend/database.sql
@@ -11,3 +11,9 @@ CREATE TABLE movies (
 
 INSERT INTO movies (movie_title, movie_director, movie_rating, movie_genre, movie_release_date) VALUES ('Transformers', 'Michael Bay', 7, 'Action', '2007-07-12');
 INSERT INTO movies (movie_title, movie_director, movie_rating, movie_genre, movie_release_date) VALUES ('John Wick: Chapter 3 - Parabellum', 'Chad Stahelski', 8, 'Action', '2019-05-19');
+INSERT INTO movies (movie_title, movie_director, movie_rating, movie_genre, movie_release_date) VALUES ('Harry Potter and the Philosophers Stone', 'Chris Columbus', 7, 'Fantasy', '2001-04-10');
+INSERT INTO movies (movie_title, movie_director, movie_rating, movie_genre, movie_release_date) VALUES ('Avengers: Endgame', 'Anthony Russo', 9, 'Action', '2019-04-26');
+INSERT INTO movies (movie_title, movie_director, movie_rating, movie_genre, movie_release_date) VALUES ('Kill Bill: Vol. 1', 'Quentin Tarantino', 8, 'Crime', '2003-09-29');
+INSERT INTO movies (movie_title, movie_director, movie_rating, movie_genre, movie_release_date) VALUES ('The Conjuring', 'James Wan', 6, 'Horror', '2013-07-19');
+INSERT INTO movies (movie_title, movie_director, movie_rating, movie_genre, movie_release_date) VALUES ('How to Train Your Dragon', 'Dean DeBlois', 8, 'Animation', '2010-03-18');
+
